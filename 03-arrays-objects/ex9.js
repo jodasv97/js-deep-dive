2af89989a456d96e3b0a372cd230820a244109e8
José Daniel Solís V. <vampirowskycr18@outlook.com>
2025-09-01 18:58:37 -0600

    Revert "feat: last exercise of arrays-objects and two exercises of async prog…"

--- 03-arrays-objects/ex9.js
+++ 03-arrays-objects/ex9.js
@@ -4,20 +4,8 @@
  * - With reduce, map an array to an object { [id]: object }
  */
 
-const persons = [
-  { id: 1, name: "Ana" },
-  { id: 2, name: "Luis" },
-  { id: 3, name: "Marta" }
-];
-
 function indexById(arr) {
   // TODO
-  const indexedArr = {};
-  arr.forEach(el => {
-    indexedArr[el.id] = el;
-  }); 
-  return indexedArr;
 }
 
 // TODO: test logs with users
-console.log(indexById(persons));
--- 04-async/ex10.js
+++ 04-async/ex10.js
@@ -6,12 +6,8 @@
 
 function delay(ms) {
   // TODO
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 (async () => {
   // TODO: test with await delay(1000)
-  console.log('Start');
-  await delay(2000);
-  console.log('Two seconds passed');
 })();
